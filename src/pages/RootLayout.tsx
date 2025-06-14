c77bbd328113a1cb3208d95c29f131eafbf428b7
Tóth Barnabás <toth.barnabas.dev@gmail.com>
2025-06-14 21:47:13 +0200

    Add react-hot-toast

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router';
+import { Toaster } from 'react-hot-toast';
 
 export const RootLayout = () => {
   return (
     <div className="flex min-h-screen">
       <Outlet />
+      <Toaster />
     </div>
   );
 };
