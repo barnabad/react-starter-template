12685440f161342d49ef2c079875cb1fce7dcb9e
Tóth Barnabás <toth.barnabas.dev@gmail.com>
2025-06-14 21:47:13 +0200

    Add react-hot-toast

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 import { Page } from '@/components';
 import { Route } from '@/config';
+import toast from 'react-hot-toast';
 
 export const Landing = () => {
   const { t } = useTranslation();
@@ -11,6 +12,7 @@ export const Landing = () => {
     <Page className="flex flex-col items-center justify-center">
       <h1 className="text-2xl font-semibold">{t('COMMON.TITLE')}</h1>
       <Link to={Route.ABOUT}>{t('COMMON.LINK')}</Link>
+      <button onClick={() => toast.success('Success!')}>Make Toast!</button>
     </Page>
   );
 };
