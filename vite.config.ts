c6b5987bdea92bb945345a63b4184ddbaad17595
Tóth Barnabás <toth.barnabas.dev@gmail.com>
2025-06-30 23:24:07 +0200

    Add vite-plugin-eslint to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import eslint from 'vite-plugin-eslint';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), eslint()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
